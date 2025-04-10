a1d2f4d5c03fd3ad16fbf1cb0abcb2fccfd50acf
nuthankumarabinbev <nuthan.kumar-ext@ab-inbev.com>
2025-04-10 10:38:55 +0530

    comment

--- demo2/module_1.py
+++ demo2/module_1.py
@@ -1,8 +1,6 @@
 
 
 class A():
-    "Insight: ⚠️ Affected functions total 2 and they are ['module_1.foo', 'module_2.__init__']): .\n--- demo1/module_1.py\n+++ demo2/module_1.py\n@@ -1 +1 @@\n-# NOTE: This file uses updated code from module_1.py\n+class A:"
-
     def __init__(self):
         self.name = 'Class A'
 
@@ -10,7 +8,6 @@ class A():
         return f'Hello from {self.name}{punctuation}'
 
 def foo(verbose=False):
-    "Insight: ⚠️ Affected functions total 1 and they are ['module_2.__init__']): .\n--- demo1/module_1.py\n+++ demo2/module_1.py\n@@ -1 +1 @@\n-\n+def foo(verbose=False):"
     a = A()
     message = a.greet('!!!')
     if verbose:
